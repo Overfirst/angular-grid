8d30daa71b916154c344400993890f23f19a051b
Overfirst <overfirst@inbox.ru>
2021-06-22 15:00:29 +0300

    Начальное оформление страницы юзеров, удалил тестовый код

---
+++ src/app/grid-models/users/users.collection.ts
@@ -0,0 +1,388 @@
+import { IUser } from "src/app/shared/interfaces";
+
+export const users: IUser[] = [
+    {
+        name: "Slater",
+        surname: "Kelley",
+        email: "slaterkelley@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Carmella",
+        surname: "Wallace",
+        email: "carmellawallace@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Melinda",
+        surname: "Clark",
+        email: "melindaclark@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Chandra",
+        surname: "Snider",
+        email: "chandrasnider@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Heidi",
+        surname: "Willis",
+        email: "heidiwillis@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Rosa",
+        surname: "Marsh",
+        email: "rosamarsh@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Anthony",
+        surname: "Young",
+        email: "anthonyyoung@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Rutledge",
+        surname: "Beasley",
+        email: "rutledgebeasley@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Hull",
+        surname: "Schroeder",
+        email: "hullschroeder@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Leigh",
+        surname: "Fulton",
+        email: "leighfulton@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Cobb",
+        surname: "David",
+        email: "cobbdavid@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Lewis",
+        surname: "Webb",
+        email: "lewiswebb@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Tran",
+        surname: "Buchanan",
+        email: "tranbuchanan@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Hickman",
+        surname: "Wong",
+        email: "hickmanwong@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Christian",
+        surname: "Hogan",
+        email: "christianhogan@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Blake",
+        surname: "Houston",
+        email: "blakehouston@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Ernestine",
+        surname: "Ryan",
+        email: "ernestineryan@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Georgia",
+        surname: "Ball",
+        email: "georgiaball@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Nash",
+        surname: "Farley",
+        email: "nashfarley@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Mamie",
+        surname: "Russell",
+        email: "mamierussell@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Sullivan",
+        surname: "Mendez",
+        email: "sullivanmendez@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Effie",
+        surname: "Barrett",
+        email: "effiebarrett@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Jimmie",
+        surname: "Alexander",
+        email: "jimmiealexander@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Lacy",
+        surname: "Olsen",
+        email: "lacyolsen@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Martha",
+        surname: "Lucas",
+        email: "marthalucas@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Velma",
+        surname: "Alvarado",
+        email: "velmaalvarado@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Dorothy",
+        surname: "Lloyd",
+        email: "dorothylloyd@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Whitehead",
+        surname: "Singleton",
+        email: "whiteheadsingleton@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Jenkins",
+        surname: "Tillman",
+        email: "jenkinstillman@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Church",
+        surname: "Boyer",
+        email: "churchboyer@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Karin",
+        surname: "Palmer",
+        email: "karinpalmer@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Ellison",
+        surname: "Robinson",
+        email: "ellisonrobinson@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Rosanna",
+        surname: "Burnett",
+        email: "rosannaburnett@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Shelley",
+        surname: "Welch",
+        email: "shelleywelch@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Foley",
+        surname: "Lawrence",
+        email: "foleylawrence@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Blanca",
+        surname: "Talley",
+        email: "blancatalley@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Miller",
+        surname: "Dillon",
+        email: "millerdillon@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Howell",
+        surname: "Graham",
+        email: "howellgraham@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Stephens",
+        surname: "Stewart",
+        email: "stephensstewart@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Dodson",
+        surname: "Reid",
+        email: "dodsonreid@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Karina",
+        surname: "Bond",
+        email: "karinabond@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Jenifer",
+        surname: "Burns",
+        email: "jeniferburns@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Haley",
+        surname: "Sparks",
+        email: "haleysparks@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Gay",
+        surname: "Flynn",
+        email: "gayflynn@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Dianna",
+        surname: "Delgado",
+        email: "diannadelgado@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Olga",
+        surname: "Bryant",
+        email: "olgabryant@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Cote",
+        surname: "Pace",
+        email: "cotepace@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Rodgers",
+        surname: "Sexton",
+        email: "rodgerssexton@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Iris",
+        surname: "Hebert",
+        email: "irishebert@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Mays",
+        surname: "Sanders",
+        email: "mayssanders@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Duncan",
+        surname: "Morse",
+        email: "duncanmorse@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Tamara",
+        surname: "Whitfield",
+        email: "tamarawhitfield@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Robertson",
+        surname: "Clemons",
+        email: "robertsonclemons@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Patrica",
+        surname: "Gibson",
+        email: "patricagibson@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Emily",
+        surname: "Bradley",
+        email: "emilybradley@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Althea",
+        surname: "Conley",
+        email: "altheaconley@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Lois",
+        surname: "Cohen",
+        email: "loiscohen@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Jordan",
+        surname: "Blevins",
+        email: "jordanblevins@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Isabella",
+        surname: "Goodman",
+        email: "isabellagoodman@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Horton",
+        surname: "Solomon",
+        email: "hortonsolomon@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Frederick",
+        surname: "Jacobson",
+        email: "frederickjacobson@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Cummings",
+        surname: "Underwood",
+        email: "cummingsunderwood@mondicil.com",
+        avatar: "male"
+    },
+    {
+        name: "Elvira",
+        surname: "Benton",
+        email: "elvirabenton@mondicil.com",
+        avatar: "female"
+    },
+    {
+        name: "Dyer",
+        surname: "Payne",
+        email: "dyerpayne@mondicil.com",
+        avatar: "male"
+    }
+];
--- src/app/grid-models/users/users.component.ts
+++ src/app/grid-models/users/users.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild } from '@angular/core';
+import { DataBindingDirective } from "@progress/kendo-angular-grid";
+import { process } from "@progress/kendo-data-query";
+import { users } from "./users.collection";
 
 @Component({
   selector: 'app-users',
@@ -7,10 +10,40 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UsersComponent implements OnInit {
+  @ViewChild(DataBindingDirective) dataBinding: DataBindingDirective;
+  public gridData: any[] = users;
+  public gridView: any[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.gridView = this.gridData;
   }
 
+  public onFilter(event: any): void {
+    const inputValue: string = event.target.value;
+
+    this.gridView = process(this.gridData, {
+      filter: {
+        logic: "or",
+        filters: [
+          {
+            field: "name",
+            operator: "contains",
+            value: inputValue,
+          },
+          {
+            field: "surname",
+            operator: "contains",
+            value: inputValue,
+          },
+          {
+            field: "email",
+            operator: "contains",
+            value: inputValue,
+          },
+        ],
+      },
+    }).data;
+
+    this.dataBinding.skip = 0;
+  }
 }
