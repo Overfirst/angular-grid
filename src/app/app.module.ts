45830ffe2e8587f06b2ec2d47ddbcd899e7a2a28
Overfirst <overfirst@inbox.ru>
2021-06-23 12:03:26 +0300

    Удалил компонент из примера

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
-import { RatingComponent } from "./rating/rating.component";
 import { SharedModule } from "./shared/shared.module"
 
 import { UsersModule } from "./grid-models/users/users.module";
@@ -23,7 +22,6 @@ import { AppRoutingModule } from "./app.routing.module";
   ],
   declarations: [
     AppComponent,
-    RatingComponent
   ],
   bootstrap: [AppComponent],
 })
